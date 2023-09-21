ba0ee317d6a20ddb23e39fee9c620efae4d60f3d
hyle_s <samhyle@gmail.com>
2023-09-20 23:24:53 -0500

    Added refractor code and limit message

--- js/script.js
+++ js/script.js
@@ -14,14 +14,29 @@ const guestFull = document.querySelector(".alert");
 addGuestButton.addEventListener("click", function () {
     const guest = guestInput.value;
     //console.log(guest);
-    if (guest !== "")   {
-        const listItem = document.createElement("li");
-        listItem.innerText = guest;
-        guestList.append(listItem);
-    }
+    if (guest !== "")
+        addToList(guest);
+        updateGuestCount();
     clearInput();
 });
 
-const clearInput = function ()  {
+const clearInput = function () {
     guestInput.value = "";
+};
+
+const addToList = function (guest) {
+    const listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+};
+
+var updateGuestCount = function () {
+    const guests = document.querySelectorAll(".guest-list li");
+    guestCount.innerText = guests.length;
+    if (guests.length === 8) {
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("hide");
+    }
 };
